8ad8f3660ca9a31fca6d1ef500e7e84aa0456b99
Juhisaho <ahojuhis@gmail.com>
2022-04-12 19:21:39 +0300

    Oma styles kansio vittuili niin pois. Pistin mallit Login.js perään niin ainakin toimii ohjelmisto.

--- screens/login/Login.js
+++ screens/login/Login.js
@@ -6,7 +6,7 @@ import LoginGoogle from "../../components/buttons/LoginGoogle";
 import Image from '../../assets/background.jpg'
 import userContext from "../../context/user/userContext";
 import { StatusBar } from "expo-status-bar";
-import StyleSheet from "../../style/style.js"
+
 
 export default function Login({navigation}) {
 
@@ -69,3 +69,36 @@ export default function Login({navigation}) {
 
     
 }
+const styles = StyleSheet.create({
+    button: {
+        marginBottom: 20, 
+        width: '80%'
+    },
+    textInput: {
+        width: '80%',
+        backgroundColor: 'white',
+        opacity: 0.7,
+        borderColor: 'grey',
+        borderWidth: 1,
+        marginTop: 10
+    },
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    image: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    box: {
+        height: '80%',
+        width: '90%',
+        backgroundColor: 'white',
+        opacity: 0.7,
+        borderRadius: 20,
+        alignItems: 'center' 
+    }
+})
