ab12a132d83c045292d8e0d1264f1d6c55f69482
Maxbwee <maxbowen1234@gmail.com>
2022-05-07 18:25:34 +0300

    Lisätty käyttäjän tietoa profiili sivulle

--- screens/profile/Profile.js
+++ screens/profile/Profile.js
@@ -9,37 +9,46 @@ import { db } from "../../database/firebase";
 import { Avatar, ListItem } from "react-native-elements";
 import userContext from "../../context/user/userContext"
 import tyylit from "../../tyyli/tyyli";
+import { getAuth } from 'firebase/auth';
+import { app } from '../../database/firebase'
+import DefAvatar from "../../assets/avatar.png"
 
 export default function Profile({navigation}) {
 
-    const [users, setUsers] = useState([])
+    
     const { state } = useContext(userContext)
     
-    useEffect(() => {
-        onValue(
-            ref(db, "users"), (snapshot) => {
-                const data = snapshot.val()
-                setUsers(Object.entries(data))
-            }
-        )
-    }, [])
+    const auth = getAuth(app)
+    const user = auth.currentUser;
+   
 
-    const userAvatar = () => {
-        const url = users.filter(user => user[0] === senderId).map(item => item[1].athlete_picture)
-        const pictureUrl = url[0]
-        
+    const renderAvatar = () => {
+
+        let picture
+
+        const userRef = ref(db, 'users/' + user.uid);
+        onValue(userRef, (snapshot) => {
+            const data = snapshot.val();
+            picture = data.athlete_picture
+            
+        })
+
+        return <Avatar rounded source={picture? {uri: picture} : DefAvatar}/>
     }
+
+    // useEffect( () => {
+    //     console.log('moi')
+    //     console.log(usersInfo)
+    // },[usersInfo])
         
     return (
         <View style={{
             flex: 1
         }}>
-            <View style={{marginTop: 20}}>
+            <View>
             <Image source={require('../../assets/stravaprofile.png')}/>               
             </View>
-            {/* <ListItem>
-            <Avatar rounded source={userAvatar}  />
-            </ListItem> */}
+            {renderAvatar()}
             <View style={tyylit.profile}>
                 <Text style={{fontSize: 25, fontWeight: 'bold'}}>Tervetuloa skabaan</Text>
             </View>
